6a59cb14fa3a691d64a20011a66b850f14e4087e
조현서 <hyeonseojo929@gmail.com>
2026-04-02 09:53:19 +0900

    CounterApp +

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,19 +1,22 @@
 //파일 구조 시험 문제
-
 import { useState } from 'react'
 // import './App.css'
 
 function CounterApp() {
-  const [count, setCount] = useState(0)
+  // 상태값, 이전 상태값 setCount는 count의 상태값을 변경하는 함수; 
+  const [count, setCount] = useState(0) 
 
   return (
+    // 자바스크립트로 변환
     <>
-      <button
+    <h1>{count}</h1>
+    <button onClick={() => setCount((count) => count + 1)}>+</button>
+      {/* <button
         className="counter"
-        onClick={() => setCount((count) => count + 1)}
+        onClick={() => setCount((count) => count + 1)} 
       >
         Count is {count}
-      </button>
+      </button> */}
     </>
   )
 }
