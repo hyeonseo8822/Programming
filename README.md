abd3b5282fd0b0c55a91a83df952ee3fff8bea4e
조현서 <hyeonseojo929@gmail.com>
2026-04-09 09:07:18 +0900

    CountApp +(최대10끼지) 끝

--- README.md
+++ README.md
@@ -8,3 +8,7 @@ npm create vite@latest .
 ```
 
 ### CounterApp
+ - `useState(초기값)`
+ - 'onClick = {() => set함수(바꿀값)}`
+ - 'onClick = {() => set함수((이전state) => 이전state + 1)}`
+ - 'onClick = {함수이름}`
--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,19 +1,26 @@
+//https://programming.s2418.workers.dev/
 //파일 구조 시험 문제
 import { useState } from 'react'
 // import './App.css'
 
 function CounterApp() {
   // 상태값, 이전 상태값 setCount는 count의 상태값을 변경하는 함수; 
-  const [count, setCount] = useState(0) 
+  const [count, setCount] = useState(0)
 
+  const plusMax10 = () => {
+    setCount((count) => Math.min(count + 1, 10))
+  } 
   return (
     // 자바스크립트로 변환
     <>
-    <h1>{count}</h1>
-    <button onClick={() => setCount((count) => count + 1)}>+</button>
-    <button onClick={() => setCount((count) => count + 2)}>+2</button>
-    <button onClick={() => setCount((count) => count - 1)}>-</button>
-    <button onClick={() => setCount(0)}>reset</button>
+      <h1>{count}</h1>
+      <button onClick={() => setCount((count) => count + 1)}>+</button>
+      <button onClick={() => setCount((count) => count + 2)}>+2</button>
+      <button onClick={() => setCount((count) => count - 1)}>-</button>
+      <button onClick={() => setCount((count) => Math.min(count + 1, 10))}>+(최대10까지)</button>
+      <button onClick={() => plusMax10()}> +(최대10까지)</button >
+      <button onClick={() => setCount(0)}>reset</button>
+      {/* <button onClick={setCount(0)}>reset</button> */}
       {/* <button
         className="counter"
         onClick={() => setCount((count) => count + 1)} 
