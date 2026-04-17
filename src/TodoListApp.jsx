8ac951a95b57be3590edc13280a02a2e3f17e724
조현서 <hyeonseojo929@gmail.com>
2026-04-17 14:28:39 +0900

    TodoListApp()

--- src/TodoListApp.jsx
+++ src/TodoListApp.jsx
@@ -2,13 +2,28 @@ import './todoList.css'
 import TodoHeader from './components/TodoHeader.jsx'
 import TodoAdder from './components/TodoAdder.jsx'
 import TodoList from './components/TodoList.jsx'
+import { useState } from 'react';
+
+class Todo {
+    constructor(text) {
+        this.id = Date.now() // 고유의 값. new Date().getTime()와 동일  
+        this.text = text // 할 일 내용
+        this.Iscompleted = false; //완료 여부 : 미완
+    }
+}
 
 function TodoListApp() {
+    const[todos, setTodos] = useState([]); // 할일 목록 저장 state, 기본값은 빈 배열
+
+    function addTodo(text) {
+        setTodos((todos) =>[...todos,new Todo(text)]);
+    }
+
     return (
         <div className="todo">
             <TodoHeader />
-            <TodoAdder />
-            <TodoList />
+            <TodoAdder onAddTodo={addTodo} />
+            <TodoList todos={todos} />
         </div>
     )
 }
--- src/components/TodoAdder.jsx
+++ src/components/TodoAdder.jsx
@@ -1,10 +1,25 @@
 import Button from './Button.jsx'
+import { useState } from 'react';
 
-export default function TodoAdder(props) {
-    const { onAdd } = props
+export default function TodoAdder({ addTodo }) {
+    const [inputTodo, setInputTodo] = useState(''); // input 요소의 value를 저장하는 state
+    //사용자가 입력하는 것은 항상 주의 필요!
+    //사용자입력 : 마음대로
+    if (!inputTodo.trim()) { //trim() : 문자열 양쪽 공백 제거
+        return;
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault(); //submit 이벤트의 기본 동작인 페이지 새로고침 방지
+        //addTodo(Text)
+        //text: 1. input 요소의 value, 2. input value를 state
+        addTodo(inputTodo.trim()); //trim() : 문자열 양쪽 공백 제거
+        setInputTodo('');
+    };
+
     return (
-        <form className="todo__form" onSubmit={onAdd}>
-            <input type="text" placeholder="할 일을 입력하세요" className='todo__input' />
+        <form className="todo__form" onSubmit={handleSubmit}>
+            <input type="text" placeholder="할 일을 입력하세요" className='todo__input' value={inputTodo} onChange={(event) => setInputTodo(event.target.value)} />
             <Button type="submit" className="todo__button todo_button--add">
                 +
             </Button>
