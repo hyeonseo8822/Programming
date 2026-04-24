291e715e1e16672956e91eef5a97208fcb09ceee
조현서 <hyeonseojo929@gmail.com>
2026-04-24 14:12:36 +0900

    TodoListApp editTodo()

--- src/TodoListApp.jsx
+++ src/TodoListApp.jsx
@@ -39,11 +39,17 @@ function TodoListApp() {
     setTodos((todos) => todos.filter((todo) => todo.id !== id));
   }
   // function addTodo(text) { setTodos((todos) => [...todos, new Todo(text)])}
+
+  function editTodo(id, newText) {
+    //todos에서 하나씩 꺼내어 todo.id가 id와 같으면 text를 newText로 대입하자
+    setTodos((todos) => todos.map((todo) => (todo.id === id ? { ...todo, text: newText } : todo)));
+  }
+
   return (
     <div className="todo">
       <TodoHeader />
       <TodoAdder addTodo={addTodo} />
-      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} editTodo={editTodo} />
     </div>
   )
 }
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,13 +1,53 @@
 import Checkbox from './Checkbox.jsx';
 import Button from './Button.jsx';
+import { useState } from 'react';
 
-export default function TodoItem({ deleteTodo, todo, toggleTodo }) {
+export default function TodoItem({ deleteTodo, editTodo, todo, toggleTodo }) {
+    //isEditing: 편집 모드 여부 state
+    const [isEditing, setIsEditing] = useState(false);
+    //editText: 편집할 때 input 요소에 들어갈 text state
+    const [editText, setEditText] = useState(todo.text);
+    //수정중이 아니면 -> 수정중, <input />
+    function handleEditClick() {
+        if (!isEditing) {
+            setIsEditing(true);
+            setEditText(todo.text);
+        } else {
+            const trimmedText = editText.trim();
+            if (trimmedText !== "" && trimmedText !== todo.text) {
+                editTodo(todo.id, trimmedText);
+            }
+            setIsEditing(false);
+        }
+    }
+
     return (
         <li className={`todo__item${todo.isCompleted ? " todo__item--complete" : ""}`}>
-            <Checkbox id={todo.id} onChange={() => toggleTodo(todo.id)}>
-                {todo.text}
-            </Checkbox>
-            <Button className="todo__button todo__button--edit">✏️</Button>
+            {/*수정중이 아니면*/}
+            {!isEditing && (
+                <Checkbox
+                    id={todo.id}
+                    checked={todo.isCompleted}
+                    onChange={() => toggleTodo(todo.id)}
+                >
+                    {todo.text}
+                </Checkbox>
+            )}
+            {/* 수정중이면*/}
+            {isEditing && (
+                <input
+                    type="text"
+                    className="todo__edit-input"
+                    value={editText}
+                    autoFocus
+                    onChange={(e) => setEditText(e.target.value)}
+                    onBlur={handleEditClick}
+                    onKeyDown={(e) => e.key === "Enter" && handleEditClick()}
+                />
+            )}
+            <Button className="todo__button todo__button--edit" onClick={handleEditClick}>
+                {isEditing ? "✔️" : "✏️"}
+            </Button>
             <Button className="todo__button todo__button--delete" onClick={() => deleteTodo(todo.id)}>
                 ❌
             </Button>
