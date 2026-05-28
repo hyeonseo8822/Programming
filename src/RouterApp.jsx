b7f171f80c53d548de1e9eb8101a34701173da0f
조현서 <hyeonseojo929@gmail.com>
2026-05-28 09:56:11 +0900

    useNavigate

--- src/RouterApp.jsx
+++ src/RouterApp.jsx
@@ -1,15 +1,19 @@
 import { Link } from "react-router-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import CounterApp from "./CounterApp.jsx";
 import TodoListApp from "./TodoListApp.jsx";
 
 function LinkButtonPageApp() {
+    const navigate = useNavigate();
+    
     return (
         <div>
             <h1>App 목록</h1>
             <ul>
                 <li><Link to="/counterapp">CounterApp</Link></li>
                 <li><Link to="/todolistapp">TodoListApp</Link></li>
+                <li><button onClick={() => navigate("/counterapp")}>CounterApp</button></li>
+                <li><button onClick={() => navigate("/todolistapp")}>TodoListApp</button></li>
             </ul>
         </div>
     )
